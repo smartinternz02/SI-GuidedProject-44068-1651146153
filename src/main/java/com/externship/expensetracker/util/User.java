3844f71e5554cccbfdcaf4659fc5011692dfe012
laava <laavart11@gmail.com>
2022-05-20 17:30:49 +0530

    Committed By Laavart

---
+++ src/main/java/com/externship/expensetracker/util/User.java
@@ -0,0 +1,49 @@
+package com.externship.expensetracker.util;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private String email;
+    private String firstname, lastname;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getName() {
+        return firstname+" "+lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
