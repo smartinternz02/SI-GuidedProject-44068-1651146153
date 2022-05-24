9c97a27aeb5724b9d8dd16a78d2a953a837d9757
laava <laavart11@gmail.com>
2022-05-24 12:42:45 +0530

    Committed By Laavart

--- src/main/java/com/externship/expensetracker/repository/CategoryRepository.java
+++ src/main/java/com/externship/expensetracker/repository/CategoryRepository.java
@@ -1,8 +1,8 @@
 package com.externship.expensetracker.repository;
 
-import com.externship.expensetracker.model.User;
+import com.externship.expensetracker.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository  extends JpaRepository<User, String> {}
+public interface CategoryRepository  extends JpaRepository<Category, String> {}
