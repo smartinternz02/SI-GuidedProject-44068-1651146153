196eb17f5cbfd3a6bac1c2f91de85d3329ef8cac
laava <laavart11@gmail.com>
2022-05-25 20:48:24 +0530

    Committed By Laavart

--- src/main/java/com/externship/expensetracker/controller/ExpenseTrackerController.java
+++ src/main/java/com/externship/expensetracker/controller/ExpenseTrackerController.java
@@ -3,7 +3,9 @@ package com.externship.expensetracker.controller;
 import com.externship.expensetracker.repository.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,35 +20,67 @@ public class ExpenseTrackerController {
     @Autowired
     UserRepository userRepository;
 
-    long id;
+    long userId;
+    int netBalance;
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
 
     @RequestMapping("/")
-    public String indexPage() {
+    public String displayIndexPage() {
         return "redirect:/user_login";
     }
 
     @RequestMapping("/user_register")
-    public String registerPage() {
+    public String displayRegistrationPage() {
         return "user_register";
     }
 
     @RequestMapping("/user_login")
-    public String logInPage() {
+    public String displayLogInPage() {
         return "user_login";
     }
 
-    @RequestMapping("/expenses_add")
-    public String addExpensesPage() {
-        return "expenses_add";
+    @RequestMapping("/expense_add")
+    public String displayExpenseAddPage() {
+        return "expense_add";
     }
 
-    @RequestMapping("/expenses_view")
-    public String viewExpensesPage() {
-        return "expenses_view";
+    @RequestMapping("/expense_view")
+    public String displayExpenseViewPage() {
+        return "expense_view";
     }
 
     @RequestMapping("/balance_add")
-    public String addBalancePage() {
+    public String displayBalanceAddPage() {
         return "balance_add";
+    }
+
+    @RequestMapping("/balance_view")
+    public String displayBalanceViewPage() {
+        return "balance_view";
+    }
+
+    @RequestMapping("/analysis")
+    public String displayAnalysisPage() {
+        return "analysis";
+    }
+
+    @RequestMapping("/analysis_week")
+    public String displayAnalysisWeekPage() {
+        return "analysis_week";
+    }
+
+    @RequestMapping("/analysis_category")
+    public String displayAnalysisCategoryPage() {
+        return "analysis_category";
+    }
+
+    @RequestMapping("/analysis_month")
+    public String displayAnalysisMonthPage() {
+        return "analysis_month";
+    }
+
+    @RequestMapping("/analysis_year")
+    public String displayAnalysisYearPage() {
+        return "analysis_year";
     }
 }
